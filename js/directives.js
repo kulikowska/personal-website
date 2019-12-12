0f90fb38fdc647a6e1ecef19b0110600644cd706
Ruby <rubykulikowska@gmail.com>
2019-12-11 17:22:17 -0800

    Update portfolio

--- js/directives.js
+++ js/directives.js
@@ -70,6 +70,64 @@ APP
         replace: false,
         templateUrl: 'html/portfolio.html',
         link: function($scope, $element, $attributes) {
+            $scope.portfolioItems = [
+                {
+                    title : 'Project 44',
+                    link  : 'https://www.project44.ca/',
+                    isLinkDirect : true,
+                    image : 'p441.png',
+                    desc  : 'Created for the Canadian Research and Mapping Association, Project \'44\ is a web based showcase of the Battle of Normandy. Centered around a map of the troop movements, it also includes fascinating unit war diaries and images. Currently in Beta.',
+                    tech  : ['React.js', 'Redux', 'Mapbox GL JS']
+                },
+                {
+                    title : 'Ditidaht',
+                    link  : 'http://www.learnditidaht.ca',
+                    isLinkDirect : true,
+                    image : 'ditidaht.png',
+                    desc  : 'The Ditidaht First Nation on Vancouver Island has only 6 fluent speakers today. In an effort to preserve the language, Learn Ditidaht was created. Ive been lucky enough to contribute to this fun, interactive, language learning tool, which is built using a combination of WordPress and React.',
+                    tech  : ['React.js', 'Redux', 'WordPress', 'PHP']
+                },
+                {
+                    title : 'International SOS',
+                    link  : false,
+                    isLinkDirect : false,
+                    image : 'sosinternational.png',
+                    desc  : 'Built in React and Leaflet, a simple application for International SOS to map travel risks worldwide, with options for data-driven styling and filtering by various categories.',
+                    tech  : ['React.js', 'Leaflet', 'SASS']
+                },
+                {
+                    title : 'Tarot Routing',
+                    link  : 'http://www.tarotrouting.com/tarot-routing',
+                    isLinkDirect : false,
+                    image : 'tarot.png',
+                    desc  : 'A route optimisation tool built for Tarot Analytics. Originally built in React and Flux, most of my work on this product involved rewriting into Redux, as well as some tweaks to their React Native app.',
+                    tech  : ['React.js', 'Redux', 'Leaflet', 'React Native']
+                },
+                {
+                    title : 'Yellowstone Wolf Map',
+                    link  : 'http://www.rubykulikowska.com/wolf-map',
+                    isLinkDirect : true,
+                    image : 'wolf-map.png',
+                    desc  : 'The Yellowstone Wolf Map is an interactive visualization of the reintroduction of wolf populations into Yellowstone National Park. Still a work in progess.',
+                    tech  : ['React.js', 'Mapbox GL JS', 'LESS']
+                },
+                {
+                    title : 'BirdieFire',
+                    link  : false,
+                    isLinkDirect : false,
+                    image : 'birdiefire.png',
+                    desc  : 'BirdieFire is an application used by golfers and their coaches to input and review their game data. Built in Mapbox.js and React, users get to drag their shots around the course for easy data input and immediate shot statistics.',
+                    tech  : ['React.js', 'Mapbox GL', 'LESS']
+                },
+                {
+                    title : 'Blacktie Skis Landing Page',
+                    link  : 'http://www.blacktieslopeside.com',
+                    isLinkDirect : true,
+                    image : 'blacktie.png',
+                    desc  : 'A little landing page built for Black Tie Ski Rentals of Whistler, BC.',
+                    tech  : ['HTML5', 'CSS3', 'JavaScript']
+                }
+            ]
         }
     } 
  }])
